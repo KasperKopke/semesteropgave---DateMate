c13703b54a83cb5292cbd79390ae0fae781c0346
KasperKopke <kasper@kopke.info>
2023-09-26 13:02:40 +0200

    swipe effekt på user btn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const profileWindow = document.querySelector(".profile-window");
-const profileBtn = document.querySelector(".fa-user");
+const profileBtn = document.querySelector(".user");
 
 profileBtn.addEventListener("click", () => {
   profileWindow.classList.toggle("active");
+  profileBtn.classList.toggle("active");
 });
